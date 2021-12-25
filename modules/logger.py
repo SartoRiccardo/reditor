9077a5051d0f1e1a8176ea1a27a18d9f24924428
SartoRiccardo <rikki.sarto@gmail.com>
2021-12-25 20:43:52 +0100

    Discord API limits

--- modules/logger.py
+++ modules/logger.py
@@ -1,5 +1,10 @@
 import backend.database
 import requests
+from datetime import datetime, timedelta
+import time
+
+
+last_log = datetime.now()
 
 
 class Logger:
@@ -10,9 +15,22 @@ class Logger:
 
     @staticmethod
     def log(message, severity):
+        global last_log
+        if datetime.now() < last_log+timedelta(seconds=3):
+            time.sleep(3)
+        last_log = datetime.now()
+
+        title = "Info"
+        if severity == Logger.SUCCESS:
+            title = "Success"
+        elif severity == Logger.ERROR:
+            title = "Error"
+        elif severity == Logger.WARN:
+            title = "Warning"
+
         webhook_url = backend.database.config("rdt_logger")
         embed = {"embeds": [{
-            "title": "Error",
+            "title": title,
             "color": severity,
             "description": message
         }]}
